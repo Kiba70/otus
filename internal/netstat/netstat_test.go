0a366bb1322c42a31b695f69ad8be384fa246181
Kiba70 <khodus@inbox.ru>
2025-03-23 22:58:09 +0300

    23.03

--- internal/netstat/netstat_test.go
+++ internal/netstat/netstat_test.go
@@ -5,7 +5,8 @@ import (
 	"time"
 )
 
-const netstatInput = `Active Internet connections (servers and established)
+const (
+	netstatInput = `Active Internet connections (servers and established)
 Proto Recv-Q Send-Q Local Address           Foreign Address         State       User       Inode      PID/Program name    
 tcp        0      0 127.0.0.1:46749         0.0.0.0:*               LISTEN      hev        184402348  1793155/code-384ff7 
 tcp        0      0 0.0.0.0:10050           0.0.0.0:*               LISTEN      zabbix     175588060  3664077/zabbix_agen 
@@ -22,6 +23,22 @@ udp        0      0 127.0.0.1:53            0.0.0.0:*                           
 udp        0      0 0.0.0.0:111             0.0.0.0:*                           root       20471      1/systemd           
 udp        0      0 0.0.0.0:54558           0.0.0.0:*                           root       28239      1151/rsyslogd       
 udp        0      0 127.0.0.1:323           0.0.0.0:*                           root       26288      1038/chronyd        `
+	netstatInput2 = `Active Internet connections (servers and established)
+Proto Recv-Q Send-Q Local Address           Foreign Address         State       User       Inode      PID/Program name
+tcp        0      0 10.255.255.254:53       0.0.0.0:*               LISTEN      root       20490      -
+tcp        0      0 127.0.0.1:37227         0.0.0.0:*               LISTEN      hev        28833      413/node
+tcp        0      0 127.0.0.54:53           0.0.0.0:*               LISTEN      systemd-resolve 25756      199/systemd-resolve
+tcp        0      0 127.0.0.53:53           0.0.0.0:*               LISTEN      systemd-resolve 25754      199/systemd-resolve
+tcp        0      0 0.0.0.0:23              0.0.0.0:*               LISTEN      root       21735      213/inetd
+tcp        0      0 127.0.0.1:37227         127.0.0.1:36308         ESTABLISHED hev        6667166    413/node
+tcp        0      0 127.0.0.1:36308         127.0.0.1:37227         ESTABLISHED hev        6679367    62414/node
+tcp        0      0 127.0.0.1:37227         127.0.0.1:40940         ESTABLISHED hev        6666963    522/node
+tcp        0      0 127.0.0.1:40940         127.0.0.1:37227         ESTABLISHED hev        6682094    62080/node
+udp        0      0 127.0.0.54:53           0.0.0.0:*                           systemd-resolve 25755      199/systemd-resolve
+udp        0      0 127.0.0.53:53           0.0.0.0:*                           systemd-resolve 25753      199/systemd-resolve
+udp        0      0 10.255.255.254:53       0.0.0.0:*                           root       20489      -
+udp        0      0 127.0.0.1:323           0.0.0.0:*                           root       19468      -`
+)
 
 func TestIntegrate(t *testing.T) {
 	t.Run("Запускаем parser и готовим данные", func(t *testing.T) {
